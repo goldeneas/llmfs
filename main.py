c8f9e23bc13f4307845d97e74b7bb7b10db99aa8
Nicola <nicola.picarella6@gmail.com>
2026-06-16 02:15:27 +0200

    feat: embeddings

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import tiktoken
+import torch
 from importlib.metadata import version
 from torch.utils.data import DataLoader
 
@@ -24,12 +25,14 @@ print(f"Read file {text_path}: {len(raw_text)} characters")
 
 
 context_size = 4
-stride = 4
+stride = context_size
 encoding_name = "gpt2"
-batch_size = 4
+batch_size = 8
 shuffle = True
 drop_last = True
 num_workers = 0
+torch_seed = 123
+embedding_dim = 256
 print(f"Using context size: {context_size}")
 print(f"Using stride: {stride}")
 print(f"Using encoding: {encoding_name}")
@@ -37,6 +40,8 @@ print(f"Using batch size: {batch_size}")
 print(f"Using shuffle: {shuffle}")
 print(f"Using drop last: {drop_last}")
 print(f"Using num workers: {num_workers}")
+print(f"Using torch seed: {torch_seed}")
+print(f"Using embedding dim: {embedding_dim}")
 
 tokenizer = tiktoken.get_encoding(encoding_name)
 dataset = GPTDataset(raw_text, tokenizer, context_size, stride)
@@ -47,5 +52,15 @@ dataloader = DataLoader(dataset,
                         num_workers=num_workers)
 
 data_iter = iter(dataloader)
-first_batch = next(data_iter)
-print(first_batch)
+inputs, targets = next(data_iter)
+
+torch.manual_seed(torch_seed)
+
+vocab_size = tokenizer.max_token_value + 1
+token_embedding_layer = torch.nn.Embedding(vocab_size, embedding_dim)
+token_embeddings = token_embedding_layer(inputs)
+
+position_embedding_layer = torch.nn.Embedding(context_size, embedding_dim)
+position_embeddings = position_embedding_layer(torch.arange(context_size))
+
+input_embeddings = token_embeddings + position_embeddings
